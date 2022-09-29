e7c4e82d68820ab4332a3c658925e9df0521f971
Anish <113669810+radlysamar@users.noreply.github.com>
2022-09-29 17:34:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+console.log("operators is j.s")
+// arithmatic operators
+let a = 7
+let b = 3
+console.log(a + b)
+console.log("a + b =", a + b)
+console.log("a - b =", a - b)
+console.log("a/b =", a / b)
+console.log("a*b =", a * b)
+console.log("a**b =", a ** b)
+console.log("a%b =", a % b)
+console.log("a++ =", a++)
+console.log("a =", a)
+console.log("++a", ++a)
+console.log("a-- =", a--)
+console.log("a =", a)
+console.log("++a =", ++a)
+console.log("a++ =", a++)
+console.log("--a =", --a)
+console.log("a-- =", a--)
+console.log("a =", a)
+console.log("a-- =", a--)
+// here my answer is right
+
+// assiment operators
+let d = 10
+d += 5//same as d = d + 5
+console.log("d =", d)
+d -= 5//same as d = d - 5
+console.log("d =", d)
+d *= 5//same as d = d * 5
+console.log("d =", d)
+d /= 5//same as d = d / 5
+console.log("d =", d)
+
+// comparison operators
+let comp1 = 10
+let comp2 = 5
+// let comp2 = "10";
+console.log("comp1 == comp2", comp1 == comp2)
+console.log("comp1 ≠ comp2", comp1 != comp2)
+console.log("comp1 === comp2", comp1 === comp2)
+console.log("comp1 ≠= comp2", comp1 !== comp2)
+console.log("comp1 < comp2", comp1 < comp2)
+
+//logical operators
+let x = 5;
+let y = 7;
+console.log(x < y && x == 5)
+console.log(x > y || x == 5)
+console.log(!false)
+console.log(!true)
+/* this is
+multi line
+comment with opening
+and
+closing tag*/
+// this is a single line comment
